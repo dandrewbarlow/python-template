4eebdf847fa26c18fc339dda9522a5fae2acc153
Andrew Barlow <dandrewbarlow@gmail.com>
2020-12-25 04:50:06 -0600

    initial commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+main:
+	python3 main.py
+install:
+	pip3 install -r requirements
---
+++ main.py
@@ -0,0 +1,75 @@
+# Andrew Barlow
+# https://github.com/dandrewbarlow
+# Description
+# v 0.0
+
+
+# LIBRARIES--------------------------------------------------------
+
+# http library
+# import Requests
+
+# Web Parsing Library
+# from bs4 import BeautifulSoup
+
+# Regular Expressions
+# import re
+
+# Advanced Math Functionality
+# import NumPy as np
+
+# Advanced data analysis library with many finance data scrapers
+# import Pandas as pd
+# from pandas_datareader import data
+# from pandas_datareader._utils import RemoteDataError
+
+# Plotting
+# import matplotlib
+# import plotly
+
+# Game Development
+# import Pygame
+
+# GUI toolkits
+# import pyQT
+# import PyGtk
+# from gooey import Gooey
+
+# Date & Time Library
+# import datetime
+
+# CSV Files
+# import csv
+
+# JSON Files
+# import json
+
+# Loading bar for iterables
+# from tqdm import tqdm
+
+# OS stuff, e.g. check if a file exists
+# import os
+
+# Deep Learning / Neural Networks
+# import TensorFlow
+# import Keras
+# import PyTorch
+
+# A more user friendly version of OpenAI's GPT2, with text generation in mind
+# import gpt_2_simple as gpt2
+
+# Rich - helps make pretty console apps
+# import Rich
+# also has a nice error traceback interface
+# Rich.traceback.install()
+
+# HELPER FUNCTIONS-------------------------------------------------
+
+
+
+# MAIN-------------------------------------------------------------
+def main():
+    print("Hello World")
+
+if __name__=='__main__':
+    main()
